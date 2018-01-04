b654dd3b39f530d2dd51d9bda31fec4863c8f925
KoMyo <myothantktt@gmail.com>
2018-01-04 23:08:34 +0630

    Initialize skeleton projects with default structure / value.

---
+++ app/src/main/java/com/ps/burpple/BurppleFoodApp.java
@@ -0,0 +1,16 @@
+package com.ps.burpple;
+
+import android.app.Application;
+
+/**
+ * Created by pyaesone on 1/4/18.
+ */
+
+public class BurppleFoodApp extends Application {
+    public static final String LOG_TAG = "BurppleFoodApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
